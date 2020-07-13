99d447f5620eaa4d8e66058ded84dd8a71717887
email2srini <srinivas.madiraju@outlook.com>
2020-07-13 14:52:48 -0400

    updating gthub with python input function lab

---
+++ mycode/lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+user_input = input("Please enter an IPv4 IP address:")
+
+## the line below creates a single string that is passed to print()
+# print("You told me the IPv4 address is:" + user_input)
+
+## print() can be given a series of objects separated by a comma
+print("You told me the IPv4 address is:", user_input)
+
+## Test lines that i am adding to this code
+user_input2 = input("Enter the vendor name of the device: ")
+print("Oh, So you are using", user_input2, "that's cool!")
