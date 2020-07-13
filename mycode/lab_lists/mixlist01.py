70196fa5ae3c3b63ffc7f7f66efc40bdb3429a5e
email2srini <srinivas.madiraju@outlook.com>
2020-07-13 15:23:01 -0400

    lab - lists

---
+++ mycode/lab_lists/mixlist01.py
@@ -0,0 +1,7 @@
+#!usr/bin/env python3
+
+my_list = ["sachin",11, "ganguly",5, "dravid", "laxman"]
+
+print("Openers are: ", my_list[0],"and", my_list[2])
+print("They have hit ", str(my_list[1]), " and ", str(my_list[3]), "centuries respectively")
+
